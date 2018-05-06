e04fd5a21c54491be7547a13a09a95fe4e85585f
pranavd <pranavdixit08@gmail.com>
2018-05-06 07:48:43 -0700

    connect view pager with the TabsFragmentAdapter and implement ViewPager.OnPageChangeListener

--- app/src/main/java/com/example/pranav/splitdo/MainActivity.java
+++ app/src/main/java/com/example/pranav/splitdo/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.pranav.splitdo;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+
+    private FloatingActionButton mGroupFab;
+    private FloatingActionButton mTasksFab;
+
+    private PersonalTasksFragment mFragment1;
+    private PersonalTasksFragment mFragment2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragment1 = new PersonalTasksFragment();
+        mFragment2 = new PersonalTasksFragment();//placeholder for the time being
+
+
+        mGroupFab = (FloatingActionButton) findViewById(R.id.fab_add_group);
+
+        mTasksFab = (FloatingActionButton) findViewById(R.id.fab_add_task);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.home_viewpager);
+        TabsPagerAdapter tabAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        tabAdapter.setFragments(mFragment1,mFragment2);
+
+        PagerAdapter pagerAdapter = tabAdapter;
+
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(this);
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+            case 0:
+                mGroupFab.setVisibility(View.INVISIBLE);
+                return;
+            case 1:
+                mGroupFab.setVisibility(View.VISIBLE);
+                return;
+
+            default:
+                return;
+
+        }
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
